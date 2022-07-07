4a3f7401682b47f0f2a4b38361f2ae4ee9b73ddd
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-07-07 11:59:10 -0500

    finished adding products page and connecting to best buy website

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+        //this is what is loaded when you look for Product/Index on your website
+        //the Product comes from the name of the file ProductController
+        ///you can open it with localhost:44387/Product
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();   
+            return View(products);
+        }
+
+    }
+}
